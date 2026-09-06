4ee03df0a3429a2569cc44d05b263a2b2badc129
Aaron <aaron@nixos.local>
2026-09-06 23:00:39 +0200

    feat: add extensions

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,15 +16,18 @@ return {
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "lua", "luadoc", "printf", "vim", "vimdoc",
+        "html", "css",
+        "nix", "python", "go",
+        "javascript", "typescript", "tsx",
+        "json", "yaml", "markdown", "markdown_inline",
+      },
+    },
+  },
 
   {
     "m4xshen/hardtime.nvim",
@@ -46,5 +49,39 @@ return {
       -- marks (they clutter lines with repeated characters)
       targetedMotionHints = { enabled = false },
     },
+  },
+
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {
+      modes = {
+        char = {
+          enabled = true,
+          keys = { "f", "F", "t", "T" }, -- excludes ";"/","
+        },
+      },
+    },
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Jump" },
+      { "S", mode = { "n", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
+
+  {
+    "numToStr/Comment.nvim",
+    event = "VeryLazy",
+    opts = {},
+  },
+
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    opts = {},
   },
 }
