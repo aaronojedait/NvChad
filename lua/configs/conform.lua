dc1e5c647ce80619945148dbf20c533b32216613
Aaron <aaron@nixos.local>
2026-09-06 22:59:52 +0200

    feat: add formatters

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,8 +1,16 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    python = { "ruff_format" },
+    go = { "goimports" },
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescriptreact = { "prettier" },
+    json = { "prettier" },
+    yaml = { "prettier" },
+    markdown = { "prettier" },
+    nix = { "nixfmt" },
   },
 
   -- format_on_save = {
