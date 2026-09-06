836c77908febfe8b1800e0e396be05e8100c4196
Aaron <aaron@nixos.local>
2026-09-06 23:00:02 +0200

    feat: add linters

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "pyright", "gopls", "ts_ls", "nil_ls", "jsonls", "yamlls", "marksman" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
