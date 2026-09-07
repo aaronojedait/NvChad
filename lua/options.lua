ce05bae35e969c29f8c37e74e0cc23fd110938fb
Aaron <aaron@nixos.local>
2026-09-07 15:22:44 +0200

    feat: add personal settings

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,27 @@
 require "nvchad.options"
 
--- add yours here!
+local o = vim.o
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- Numbers
+o.number = true
+o.relativenumber = true
+o.numberwidth = 3
+
+-- Navigation and editing
+o.scrolloff = 8
+o.sidescrolloff = 8
+o.smoothscroll = true
+o.inccommand = "split"
+o.virtualedit = "block"
+o.confirm = true
+
+-- Appearance
+o.winborder = "rounded"
+o.colorcolumn = "80"
+o.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Folding (treesitter)
+o.foldmethod = "expr"
+o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+o.foldlevel = 99
