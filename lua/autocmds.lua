aaa7e760db3c04bde38ae719fda735fb795edec6
Aaron <aaron@nixos.local>
2026-09-07 15:22:12 +0200

    feat: add autocmds for highlight yank and remember cursor position

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,21 @@
 require "nvchad.autocmds"
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("UserYankHighlight", { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+autocmd("BufReadPost", {
+  group = vim.api.nvim_create_augroup("UserLastPosition", { clear = true }),
+  callback = function(args)
+    local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+    local line_count = vim.api.nvim_buf_line_count(args.buf)
+    if mark[1] > 0 and mark[1] <= line_count then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
