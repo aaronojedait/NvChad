8efef366a10ef8be8b3bf874ac851fcd673d4a3c
Aaron <aaron@nixos.local>
2026-09-13 18:56:55 +0200

    feat: add recipe to load all servers with a loop

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,20 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "pyright", "gopls", "ts_ls", "nil_ls", "jsonls", "yamlls", "marksman" }
-vim.lsp.enable(servers)
+local servers = {
+  html = {},
+  cssls = {},
+  pyright = {},
+  gopls = {},
+  ts_ls = {},
+  nil_ls = {},
+  jsonls = {},
+  yamlls = {},
+  marksman = {},
+}
 
--- read :h vim.lsp.config for changing options of lsp servers 
+for name, opts in pairs(servers) do
+  vim.lsp.config(name, opts)
+  vim.lsp.enable(name)
+end
+
+-- read :h vim.lsp.config for changing options of lsp servers
